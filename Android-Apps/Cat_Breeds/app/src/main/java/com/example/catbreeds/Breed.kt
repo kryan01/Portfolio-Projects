dff0418ecdd47988fdb5309a26c1a61401a07665
Kayla <kryan01@jaguar.tamu.edu>
2026-02-24 13:05:35 -0600

    Delete Android-Apps/Cat_Breeds directory

--- Android-Apps/Cat_Breeds/app/src/main/java/com/example/catbreeds/Breed.kt
+++
@@ -1,9 +0,0 @@
-package com.example.catbreeds
-
-data class Breed(
-    val id: String,
-    val name: String,
-    val temperament: String,
-    val origin: String,
-    var imageUrl: String? = null
-)
